178d1db24598380aff2781bc52f38f2712562a8c
GerardoMAC <49138112+GerardoMAC@users.noreply.github.com>
2019-09-29 22:41:17 -0500

    Add files via upload

---
+++ Api.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Práctica_1: Capturas de pantalla del servidor y el resultado de la API
+
+# ![puppies.PNG](attachment:puppies.PNG)
+
+# ![puppies3.jpeg](attachment:puppies3.jpeg)
+
+# ![puppies2.PNG](attachment:puppies2.PNG)
+
+# In[ ]:
+
+
+
+
